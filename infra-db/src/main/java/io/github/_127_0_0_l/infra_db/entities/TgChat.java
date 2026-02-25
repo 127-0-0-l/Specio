ce7936790899ed5e80a9efe60a8211515579468d
maks <borisovets.maksim@gmail.com>
2026-02-25 20:22:40 +0300

    small entity changes

--- infra-db/src/main/java/io/github/_127_0_0_l/infra_db/entities/TgChat.java
+++ infra-db/src/main/java/io/github/_127_0_0_l/infra_db/entities/TgChat.java
@@ -3,9 +3,15 @@ package io.github._127_0_0_l.infra_db.entities;
 import jakarta.persistence.*;
 
 @Entity
-public record TgChat (
-        @Id
-        long id,
-        int lastMessageId
-) {
+public class TgChat {
+    @Id
+    public int id;
+
+    public long chatId;
+
+    public int stateId;
+
+    public int filtersId;
+
+    public int lastMessageId;
 }
