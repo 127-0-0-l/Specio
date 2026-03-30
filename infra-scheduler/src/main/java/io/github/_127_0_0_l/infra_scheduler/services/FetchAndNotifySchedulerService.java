06d6e44ff2c2eb2a47a6cf3fb1be03b4c96f87d2
maks <borisovets.maksim@gmail.com>
2026-03-30 15:28:06 +0300

    refactoring

--- infra-scheduler/src/main/java/io/github/_127_0_0_l/infra_scheduler/services/FetchAndNotifySchedulerService.java
+++ infra-scheduler/src/main/java/io/github/_127_0_0_l/infra_scheduler/services/FetchAndNotifySchedulerService.java
@@ -12,6 +12,7 @@ import io.github._127_0_0_l.infra_scheduler.adapters.in.SchedulerUseCaseService;
 import io.github._127_0_0_l.infra_scheduler.interfaces.DateTimeService;
 import io.github._127_0_0_l.infra_scheduler.interfaces.FetchAndNotifyScheduler;
 import io.github._127_0_0_l.infra_scheduler.models.NewRecordsCountLog;
+import io.github._127_0_0_l.infra_scheduler.models.NewRecordsCountLogDTO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -40,28 +41,34 @@ public class FetchAndNotifySchedulerService implements FetchAndNotifyScheduler {
     private void schedule(Long sourceId, Instant nextRunTime){
         taskScheduler.schedule(() -> {
             var result = schedulerUseCaseService.runFetchAndNotify(sourceId);
-            if (result.isPresent()){
-                interval += interval * (MAX_RECORDS_COUNT / 3 - result.get().newRecordsCount()) / MAX_RECORDS_COUNT;
 
-                if (result.get().newRecordsCount() < MAX_RECORDS_COUNT){
-                    if (sourceToLastRunTime.containsKey(sourceId)){
-                        LocalDateTime lastRun = sourceToLastRunTime.get(sourceId);
-                        var log = new NewRecordsCountLog(
-                            sourceId,
-                            dateTimeService.getFirstWeekDay(result.get().dateTime()),
-                            dateTimeService.getSecsFromFirstWeekDay(lastRun),
-                            dateTimeService.getSecsFromFirstWeekDay(result.get().dateTime()),
-                            result.get().newRecordsCount());
-                        schedulerUseCaseService.saveNewRecordsCountLog(log);
-                        sourceToLastRunTime.replace(sourceId, result.get().dateTime());
-                    } else {
-                        sourceToLastRunTime.put(sourceId, result.get().dateTime());
-                    }
+            if (result.isPresent()){
+                updateInterval(result.get());
+                if (result.get().newRecordsCount() < MAX_RECORDS_COUNT &&
+                        sourceToLastRunTime.containsKey(sourceId)){
+                    saveNewRecordsCountLog(result.get(), sourceId);
                 }
+                sourceToLastRunTime.put(sourceId, result.get().dateTime());
             }
+
             schedule(sourceId, calculateNextTime());
             log.info("schedule task for " + interval + " secconds");
         }, nextRunTime);
+    }
+
+    private void updateInterval(NewRecordsCountLogDTO nrcl){
+        interval += interval * (MAX_RECORDS_COUNT / 3 - nrcl.newRecordsCount()) / MAX_RECORDS_COUNT;
+    }
+
+    private void saveNewRecordsCountLog(NewRecordsCountLogDTO nrcl, Long sourceId){
+        LocalDateTime lastRun = sourceToLastRunTime.get(sourceId);
+        var log = new NewRecordsCountLog(
+            sourceId,
+            dateTimeService.getFirstWeekDay(nrcl.dateTime()),
+            dateTimeService.getSecsFromFirstWeekDay(lastRun),
+            dateTimeService.getSecsFromFirstWeekDay(nrcl.dateTime()),
+            nrcl.newRecordsCount());
+        schedulerUseCaseService.saveNewRecordsCountLog(log);
     }
     
     private Instant calculateNextTime(){
